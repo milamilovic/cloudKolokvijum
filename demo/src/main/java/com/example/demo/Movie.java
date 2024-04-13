ef6a3d108d87970bb8b53fd3ec41b9cf94673013
milamilovic <milamilovic03@gmail.com>
2024-04-13 11:24:26 +0200

    changed dockerfile, builds but can not run it

--- demo/src/main/java/com/example/demo/Movie.java
+++ demo/src/main/java/com/example/demo/Movie.java
@@ -1,12 +1,17 @@
 package com.example.demo;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
